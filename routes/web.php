25e60388b009311eef65ea7a3a616dad77097892
luizamaro11 <luiz.amaro90@gmail.com>
2024-10-12 16:06:43 -0300

    criando livewire components

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', WelcomeController::class);
+Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+
+Route::get('/project/{project}', [ProjectController::class, 'show'])->name('projects.show');
